b9961924bf19b927a584e2714fcc961c6c6539f6
Cristina Fuster <cristinafuster@gmail.com>
2018-08-01 15:13:10 +0200

    añadir experiencia

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-$('#myCarousel').on('slide.bs.carousel', function () {
-  direction: left;
-})
+$(window).on('scroll', retraer);
+
+			function retraer(){
+				
+				var scrollRealizado = $(window).scrollTop();
+				
+				if (scrollRealizado > 50) {
+
+					$('nav').css({
+						'line-height': '50px',
+						'height': '50px'
+					});
+				
+				} else {
+					
+					$('nav').css({
+						'line-height': '90px',
+						'height': '90px'
+					});
+
+				}
+			}
